5bfa21ce9b9f650eaf5d22ce338d0691fe652128
Ihab Ali <ihab@live.com>
2019-03-29 07:41:48 -0500

    closure.js done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,17 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function speak() {
+  let name = "Trey";
 
-
-/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
+  return function () { // begin closure II
+    console.log(`Hi: ${name}`);
+  };                  // end closure II
+}                   // end closure
+let name = "Bill";  // global scope
+let work = speak(); // global scope
+work();
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+  let count = 0;
+  const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+    return ++count;
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+ console.log(counter());
+ console.log(counter());
+ console.log(counter());
+ console.log(counter());
+
+/* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
