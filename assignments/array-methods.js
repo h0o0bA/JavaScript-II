f3a4f5ed98cb411c2da83f6ec1371550c8bdf6aa
Ihab Ali <ihab@live.com>
2019-03-29 07:39:18 -0500

    array-methods.js done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,26 +58,65 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 let fullName = [];
 console.log(fullName);
 
+runners.forEach((runner) => {
+   fullName.push(`${runner.first_name} ${runner.last_name}`);
+  })
+
+  console.log(fullName);
+
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
-console.log(allCaps); 
+
+runners.map((runner) => {
+   allCaps.push(runner.first_name.toUpperCase());
+  })
+
+console.log(allCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+
+runners.filter((runner) => {
+   if (runner.shirt_size === "L") {
+    largeShirts.push(`${runner.first_name} ${runner.last_name}`);
+   }
+  })
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+let ticketPriceTotal = runners.reduce(function(value, currentEntry) {
+    return value + currentEntry.donation;
+   }, 0);
+ 
+ console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 List out runners last name as well as their shit sizes
 
-// Problem 2
-
-// Problem 3
+const shirtList = runners.map((runner) => {
+    return `${runner.last_name}: ${runner.shirt_size}`;
+   })
+   
+   console.log(shirtList.sort().join("\n"));
+   
+   // Problem 2 List participant's email addresses
+   
+   let eList = runners.map((val) => {
+       return val.email;
+   });
+   
+   console.log(eList);
+   
+   // Problem 3 List the donation amounts
+   
+   let donationList = runners.map((val) => {
+       return val.donation;
+   });
+   
+   console.log(donationList);
